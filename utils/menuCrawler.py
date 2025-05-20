d6f014cbd182876eb958fae4e75db57fe2d20b77
Ian Kim <ruhyun92@gmail.com>
2025-05-20 17:24:59 +0900

    코드 개편 시작

--- utils/menuCrawler.py
+++ utils/menuCrawler.py
@@ -73,6 +73,7 @@ try:
     time.sleep(1)
 
     # 메뉴 항목 추출
+    # 메뉴 항목 아이템들 가져오지 못함. 해당 부분 수정 요망
     menu_items = menu.find_elements(
         By.CSS_SELECTOR,
         "#app-root > div > div > div > div:nth-child(6) > div:nth-child(2) > div.place_section.no_margin > div.place_section_content > ul",
@@ -104,63 +105,3 @@ except Exception as e:
 
 # 드라이버 종료
 driver.quit()
-
-
-# class NaverMapCrawler:
-#     def __init__(self, driver_path):
-#         """
-#         초기화 메서드: 웹 드라이버 설정
-#         """
-#         self.driver = webdriver.Chrome(executable_path=driver_path)
-#         self.driver.implicitly_wait(10)  # 암시적 대기 시간 설정
-
-#     def search_place(self, keyword):
-#         """
-#         네이버 맵에서 키워드로 검색
-#         """
-#         self.driver.get("https://map.naver.com/")
-#         time.sleep(2)  # 페이지 로딩 대기
-
-#         # 검색창 찾기
-#         search_box = self.driver.find_element(By.CSS_SELECTOR, "input#search-input")
-#         search_box.send_keys(keyword)  # 키워드 입력
-#         search_box.send_keys(Keys.RETURN)  # 엔터 키 입력
-#         time.sleep(2)  # 검색 결과 대기
-
-#     def select_first_store(self):
-#         """
-#         검색 결과 중 첫 번째 가게 선택
-#         """
-#         # 첫 번째 검색 결과 클릭
-#         first_store = self.driver.find_element(By.CSS_SELECTOR, "li.lsnx")
-#         first_store.click()
-#         time.sleep(2)  # 가게 페이지 로딩 대기
-
-#     def crawl_menu(self):
-#         """
-#         가게의 메뉴 정보 크롤링
-#         """
-#         # 메뉴 탭 클릭 (네이버 맵의 메뉴 탭 선택자)
-#         menu_tab = self.driver.find_element(By.CSS_SELECTOR, "div._tab_menu a[data-tab='MENU']")
-#         menu_tab.click()
-#         time.sleep(2)  # 메뉴 로딩 대기
-
-#         # 메뉴 목록 크롤링
-#         menu_items = self.driver.find_elements(By.CSS_SELECTOR, "div._1ZDCG ul li")
-#         menu_list = []
-
-#         for item in menu_items:
-#             try:
-#                 menu_name = item.find_element(By.CSS_SELECTOR, "span.lPzHi").text
-#                 menu_price = item.find_element(By.CSS_SELECTOR, "div._3yfZ1").text
-#                 menu_list.append((menu_name, menu_price))
-#             except:
-#                 continue
-
-#         return menu_list
-
-#     def close(self):
-#         """
-#         브라우저 종료
-#         """
-#         self.driver.quit()
